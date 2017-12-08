b6597704efb80cb266a398dacdc86cf375e76fd4
Joe Warren <joe@Joes-MacBook-Pro.local>
2017-12-08 11:04:45 -0600

    questions updated

--- src/components/question/question/question.js
+++ src/components/question/question/question.js
@@ -1,8 +1,6 @@
-angular.module('app')
-.controller('questionCtrl', function($scope, mainSrvc){
-    $scope.test = "connected"
-    mainSrvc.getPackages().then(response => {
-        $scope.questions = response.data
-        
-    })
-})
+angular.module("app").controller("questionCtrl", function($scope, mainSrvc) {
+  $scope.test = "connected";
+  mainSrvc.getQuestions().then(response => {
+    $scope.questions = response.data;
+  });
+});
